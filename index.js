c8a8895b243a17375d0bba9c17e9803ff347162c
Hugo Larcher <hugo.larcher@gmail.com>
2017-11-06 19:15:29 +0100

    Use of speaker accessory with volume control

--- index.js
+++ index.js
@@ -17,12 +17,30 @@ function RadioAccessory(log, config) {
   this.player = null;
   this.state = false;
   
-  this.service = new Service.Switch(this.name);
+  this.service = new Service.Speaker(this.name);
   
   this.service
-    .getCharacteristic(Characteristic.On)
+    .getCharacteristic(Characteristic.Mute)
     .on('get', this.getState.bind(this))
     .on('set', this.setState.bind(this));
+
+  this.service
+    .getCharacteristic(Characteristic.Volume)
+    .on('get', this.setVolume.bind(this))
+    .on('set', this.setVolume.bind(this));
+}
+
+RadioAccessory.prototype.getVolume = function(callback) {
+  this.log('getting current volume');
+  this.amixer = 'awk -F"[][]" \'/dB/ { print $2 }\' <(amixer sget PCM)';
+  this.amixer.stdout.setEncoding('utf8');
+  this.player.stdout.on('data', function(data){callback(null, data.replace('%',''));});
+}
+
+RadioAccessory.prototype.setVolume = function(volume, callback) {
+  this.log('setting volume to ' + volume + '%');
+  this.amixer = 'amixer cset numid=1 ' + volume + '%';
+  callback(null);
 }
 
 RadioAccessory.prototype.getState = function(callback) {
